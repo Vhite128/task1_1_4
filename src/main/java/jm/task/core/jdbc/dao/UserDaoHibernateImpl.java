cdf7ebde991a71e1dcf8011ecab5f6ca1a7d193a
Vhite128 <vhite@internet.ru>
2024-12-05 23:41:18 +0300

    12.24

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -4,10 +4,11 @@ import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.util.Util;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-
 import java.util.List;
 
+
 public class UserDaoHibernateImpl implements UserDao {
+
 
     @Override
     public void createUsersTable() {
